a298356820566bc9f123d2dcd482e6d22525e46b
vickylcodes <vickylee10@hotmail.com>
2022-11-17 15:25:21 +1000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Matt Delac and is {""}
-        <a
-          href="https://github.com/vickylcodes/demo-react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div class="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Matt Delac and is {""}
+          <a
+            href="https://github.com/vickylcodes/demo-react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
