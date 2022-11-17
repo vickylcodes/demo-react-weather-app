4f0d888c29c7a51f48ec2a7048efca241e7ec1e8
vickylcodes <vickylee10@hotmail.com>
2022-11-17 15:03:14 +1000

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Matt Delac and is {""}
+        <a
+          href="https://github.com/vickylcodes/demo-react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
