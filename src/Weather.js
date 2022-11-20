f0ad3f38e8ad154935e5e1b31cf46b8f5a70bbfc
vickylcodes <vickylee10@hotmail.com>
2022-11-20 21:12:28 +1000

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -16,9 +16,7 @@ export default function Weather(props) {
       humidity: response.data.temperature.humidity,
       date: new Date(response.data.time * 1000),
       description: response.data.condition.description,
-      icon: (
-        <img href="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/scattered-clouds-night.png" />
-      ),
+      icon: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon_url}.png`,
       wind: response.data.wind.speed,
       city: response.data.city,
     });
