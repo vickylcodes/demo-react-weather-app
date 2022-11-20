4c3f3d4d6da8aaaa806e697b1fd7ddf4ab2df12e
vickylcodes <vickylee10@hotmail.com>
2022-11-20 17:19:42 +1000

    Added rel="noreferrer" to link in App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default function App() {
           <a
             href="https://github.com/vickylcodes/demo-react-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             open-sourced on GitHub
           </a>
