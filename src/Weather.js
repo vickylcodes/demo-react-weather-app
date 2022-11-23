85db47d96ca1074ba638e80ca35ddaf3b89d834e
vickylcodes <vickylee10@hotmail.com>
2022-11-23 16:13:58 +1000

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -18,8 +18,6 @@ export default function Weather(props) {
       humidity: response.data.temperature.humidity,
       date: new Date(response.data.time * 1000),
       description: response.data.condition.description,
-          alt={response.data.condition.description}
-        />
       icon: response.data.condition.icon_url,
       wind: response.data.wind.speed,
       city: response.data.city,
